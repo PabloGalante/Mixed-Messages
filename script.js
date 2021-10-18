26c233cc1e916c55c84aa6974e351ea2100d9286
Pablo Galante <pablogalante@MacBook-Pro-de-Pablo-2.local>
2021-10-18 19:09:36 -0300

    adding randomizer and sentence maker

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('CookieYum');
+//Create OBJECT with arrays of possibilities
+const dataobject = {
+    sabores: ['vainilla', 'chocolate', 'frambuesa'],
+    rellenos: ['chocolate', 'nutella', 'dulce de leche'],
+    bebidas: ['cafe', 'te', 'mate']
+};
+
+//Generate random numbers to fetch array info
+function randomizer(property) {
+    return Math.floor(Math.random() * 3);
+}
+
+console.log(`Hoy es un gran dia para comer una cookie de ${dataobject.sabores[randomizer()]} 
+            rellena de ${dataobject.rellenos[randomizer()]} 
+            con un ${dataobject.bebidas[randomizer()]}`
+            );
